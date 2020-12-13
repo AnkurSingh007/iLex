97685712bcce7798d01c6afdeb5e5f5e070b8201
ankur singh <rushikeshi.potdar93@gmail.com>
2020-12-13 11:23:43 +0800

    Add data model for the service.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,32 @@
-INSERT INTO users (username, firstname, lastname) VALUES
-  ('lala', 'lala', 'lala'),
-  ('lolo', 'lolo', 'lolo');
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Consumer;
+DROP TABLE IF EXISTS Product;
+DROP TABLE IF EXISTS Tag;
+
+Create Table Consumer(
+  consumer_id int,
+  name varchar(255) not null,
+  PRIMARY KEY (consumer_id)
+);
+
+Create Table Orders(
+  consumer_id int,
+  order_id int not null,
+  PRIMARY KEY (consumer_id, order_id),
+  Foreign key (consumer_id) references Consumer(consumer_id)
+);
+
+Create Table Tag(
+  tag_id int ,
+  name varchar(20) not null,
+  PRIMARY KEY (tag_id)
+);
+
+Create Table Product(
+  product_id int,
+  tag_id int,
+  name varchar(20)  not null,
+  PRIMARY KEY (product_id),
+  Foreign key (tag_id) references Tag(tag_id)
+);
+
